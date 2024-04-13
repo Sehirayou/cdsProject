44dfa5cb1c443e5f35f366173da00a5649985c58
isunnyrock <isunnyrock@gmail.com>
2024-04-13 11:00:21 +0530

    pushing image upload iter2

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Hello World")
+st.title("Group2-CDS Batch 6: Final Project-Plant Disease Detection")
 
 
 upload= st.file_uploader('Upload Leaf image for disease detection', type=['png','jpg'])
